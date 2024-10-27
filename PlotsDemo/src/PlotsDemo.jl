0f2f5bccdea6e439344382d4775d899ef6eced54
JonasEbbinghaus25 <jonas.ebbinghaus25@gmail.com>
2024-10-27 21:08:53 +0100

    init

--- PlotsDemo/src/PlotsDemo.jl
+++ PlotsDemo/src/PlotsDemo.jl
@@ -1,5 +1,14 @@
 module PlotsDemo
 
-greet() = print("Hello World!")
+import Plots: plot
 
-end # module PlotsDemo
+# Data 
+x = 1:10
+y = rand(10)
+
+# Diagramm
+function create_plot()
+    plot(x, y, label="Zufallsdaten", title="Ein einfaches Diagramm", xlabel="X-Achse", ylabel="Y-Achse")
+end
+
+end 
