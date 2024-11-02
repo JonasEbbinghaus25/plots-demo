4935ee9da63fb5a8689b76ed1d87782586a2e4bd
JonasEbbinghaus25 <jonas.ebbinghaus25@gmail.com>
2024-11-02 13:33:55 +0100

    commit

---
+++ PlotsDemo/src/BasicCommands.jl
@@ -0,0 +1,20 @@
+using Plots
+
+# Simple Plot
+x = 1:10
+y = rand(10)
+plot(x, y)
+
+# Add legend
+# plot(x, y, title="Mein erster Plot", xlabel="x-Achse", ylabel="y-Achse")
+
+
+y2 = rand(10)
+# plot(x, y, label="Daten 1", legend=:topright)
+# plot!(x, y2, label="Daten 2")  # Das Ausrufezeichen fügt zum bestehenden Plot hinzu
+
+scatter(x, y)
+
+# bar(x, y)
+
+# histogram(randn(1000))
